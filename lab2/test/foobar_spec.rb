8328850c306398443147aebdb111b85f5b7cb328
uselesssas <sas.clicking@mail.ru>
2021-10-11 16:53:19 +0300

    modification of tests for lab2

--- lab2/test/foobar_spec.rb
+++ lab2/test/foobar_spec.rb
@@ -6,11 +6,11 @@ describe '#foobar' do
     expect(foobar(10, 10)).to eq(20)
   end
 
-  it 'If foo == 20' do
+  it 'If foo = 20' do
     expect(foobar(20, 10)).to eq(10)
   end
 
-  it 'If bar == 20' do
+  it 'If bar = 20' do
     expect(foobar(10, 20)).to eq(10)
   end
 end
--- lab2/test/greeting_spec.rb
+++ lab2/test/greeting_spec.rb
@@ -2,27 +2,37 @@ require 'rspec'
 require_relative '../greeting'
 
 describe Greeting do
-  it '#greeting' do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('даниил', 'Доманецкий', 21)
-    expect do
-      Greeting.new.greeting
-    end.to_not output('Привет, Даниил Доманецкий. Самое время заняться делом!').to_stdout
+  context '#greeting' do
+    it 'should do ok for age > 18' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return('даниил', 'доманецкий', 21)
+      expect do
+        Greeting.new.greeting
+      end.to_not output('Привет, Даниил Доманецкий. Самое время заняться делом!').to_stdout
+    end
 
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('Даниил', 'доманецкий', 17)
-    expect do
-      Greeting.new.greeting
-    end.to_not output('Привет, Даниил Доманецкий.' \
-                              'Тебе меньше 18 лет, но начать учится программировать никогда не рано').to_stdout
+    it 'should do ok for age < 18' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return('даниил', 'доманецкий', 17)
+      expect do
+        Greeting.new.greeting
+      end.to_not output('Привет, Даниил Доманецкий.' \
+                                 'Тебе меньше 18 лет, но начать учится программировать никогда не рано').to_stdout
+    end
   end
 
-  it '#greeting - checking exceptions' do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('', ' ', 17)
-    expect { Greeting.new.greeting }.to raise_error(StandardError)
+  context '#greeting - exception handling' do
+    it 'should be ok for empty name input' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return('', '', 17)
+      expect { Greeting.new.greeting }.to raise_error(StandardError)
+    end
 
-    allow_any_instance_of(Kernel).to receive(:gets).and_return(' ', '', 21)
-    expect { Greeting.new.greeting }.to raise_error(StandardError)
+    it 'should be ok for empty name input' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(' ', ' ', 17)
+      expect { Greeting.new.greeting }.to raise_error(StandardError)
+    end
 
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('Даниил', 'Доманецкий', -1)
-    expect { Greeting.new.greeting }.to raise_error(StandardError)
+    it 'should be ok for negative age input' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return('Даниил', 'Доманецкий', -1)
+      expect { Greeting.new.greeting }.to raise_error(StandardError)
+    end
   end
 end
